739c8e965fd33bc379c4ec542a5020eec614bb44
mehdiouanim8-prog <mehdiouanim8@gmail.com>
2026-03-04 04:44:47 +0000

    Fix import path for database module

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Text, Boolean, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from db.database import Base
+from database import Base
 
 
 class User(Base):
